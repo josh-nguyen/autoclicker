e6cc90833123b24ba0101a0c8a269d19abcddb56
Joshua Hoang Viet Nguyen <u7258344@anu.edu.au>
2023-12-08 00:37:05 +1100

    added ability to modify recording during autoclicking

--- README.md
+++ README.md
@@ -3,17 +3,23 @@ Autoclicker to simulate left mouse clicks
 
 The autoclicker has a recording phase and an autoclicking phase
 
-Recording 
-* When the program is run, all user left mouse clicks will be appended to a click list 
-* Press 'p' to pause the recording. ie: no further clicks will be added to the list until you press 'p' again
+**Press 'e' to exit the program at any time.**
+
+**Recording**
+* When run, all user left mouse clicks will be appended to a click list 
+* Press 'p' to pause/resume the recording. ie:
+once you pause, no further clicks will be added to the list until you press 'p' again to resume
+* Press 'd' to delete the last click in the recording
 * Press 'r' to restart the recording: the current click list will be deleted
 * Press 'v' to view the list of points that are in the click list
-* Press 'x' to stop recording and begin autoclicking 
+* Press 'x' to stop recording and begin autoclicking
 
-Autoclicking
-* Press 'p' to exit the program. 
-* The default sleep time between clicks is 0.5 seconds.
+**Autoclicking**
+* Press 'p' to pause/resume the autoclicks 
+  * Once paused, you are able to modify the click list:
+    * Press 'a' to toggle adding new clicks the click list 
+    * The view ,delete, and restart keybinds and functionality work the same as before
 
-Requirements
+**Requirements**
 * pyautogui, pynput, keyboard is required. 
 * to install run pip install -r requirements.txt
--- autoclicker.py
+++ autoclicker.py
@@ -1,3 +1,4 @@
+import sys
 import time
 import pyautogui
 import keyboard
@@ -8,7 +9,7 @@ class AutoClicker:
     def __init__(self):
         self.click_positions_list = []
         self.recording_active = True
-        self.autoclicking_active = True
+        self.autoclicking_active = False
         self.list_lock = Lock()
         self.add_clicks_mode = False
         self.listener = mouse.Listener(on_click=self.on_click)
@@ -17,11 +18,11 @@ class AutoClicker:
 
     def on_click(self, x, y, button, pressed):
         if pressed and self.recording_active:
-            with self.list_lock:
-                self.click_positions_list.append((x, y))
-            print(f"Clicked on {x}, {y}")
+            self.click_positions_list.append((x, y))
+            print(f"Added {x}, {y} to recording list")
 
-    def record_click_positions(self, stop_key ='x', pause_resume_key ='p', restart_key ='r', view_list_key = 'v'):
+    def record_click_positions(self, stop_key ='x', pause_resume_key ='p', restart_key ='r', view_list_key= 'v',
+    delete_last_key= 'd', exit_key = 'e'):
         print(f"Recording started. Press '{stop_key}' to stop recording.")
         print(f"Press '{pause_resume_key}' to pause/resume recording.")
         print(f"Press '{restart_key}' to restart recording.")
@@ -32,28 +33,31 @@ class AutoClicker:
                 event = keyboard.read_event(suppress=True)
                 key = event.name
                 state = event.event_type
-
-                if key == stop_key and state == keyboard.KEY_DOWN:
-                    self.recording_active = not self.recording_active
-                    break
-                elif key == pause_resume_key and state == keyboard.KEY_DOWN:
-                    self.recording_active = not self.recording_active
-                    if not self.recording_active:
-                        print("Recording paused")
+                with self.list_lock:
+                    if key == stop_key and state == keyboard.KEY_DOWN:
+                        self.recording_active = not self.recording_active
+                        self.autoclicking_active = True
+                        break
+                    elif key == pause_resume_key and state == keyboard.KEY_DOWN:
+                        self.recording_active = not self.recording_active
+                        if not self.recording_active:
+                            print("Recording paused")
+                            print(self.click_positions_list)
+                        else:
+                            print("Recording resumed")
+                            print(self.click_positions_list)
+                    elif key == restart_key and state == keyboard.KEY_DOWN:
+                        print("Restarting recording")
+                        with self.list_lock:
+                            self.click_positions_list.clear()
+                        print("Recording restarted")
+                    elif key == view_list_key and state == keyboard.KEY_DOWN:
                         print(self.click_positions_list)
-                    else:
-                        print("Recording resumed")
-                        print(self.click_positions_list)
-                elif key == restart_key and state == keyboard.KEY_DOWN:
-                    with self.list_lock:
-                        self.click_positions_list.clear()
-                    print("Recording restarted")
-                elif key == view_list_key and state == keyboard.KEY_DOWN:
-                    print(self.click_positions_list)
-                elif key == 'd' and state == keyboard.KEY_DOWN:
-                    self.click_positions_list = self.click_positions_list[:-1]
+                    elif key == delete_last_key and state == keyboard.KEY_DOWN:
+                        self.click_positions_list = self.click_positions_list[:-1]
+                    elif key == exit_key:
+                        sys.exit()
 
-            print("stopped")
         except KeyboardInterrupt:
             pass
 
@@ -62,50 +66,77 @@ class AutoClicker:
     def run_autoclicker_logic(self):
         try:
             while True:
-                if self.autoclicking_active and not self.add_clicks_mode:
-                    with self.list_lock:
+                if self.autoclicking_active == False:
+                    continue  # Exit the loop if autoclicking is not active
+
+                with self.list_lock:
+                    if self.autoclicking_active:
                         for x, y in self.click_positions_list:
                             print("Clicking at:", x, y)
                             pyautogui.click(x, y)
                             time.sleep(1)
-
-
         except KeyboardInterrupt:
             print("Autoclicker stopped.")
 
-    def run_autoclicker(self, pause_resume_key='t', add_clicks_key='a'):
+    def toggle_autoclicker(self):
+        self.autoclicking_active = not self.autoclicking_active
+        if self.autoclicking_active:
+            self.recording_active = False
+            print("Autoclicker resumed.")
+        else:
+            print("Autoclicker paused.")
+
+    def handle_additional_keys(self,additional_key,add_clicks_key = 'a',
+    delete_last_key='d',pause_resume_key = 'p' ,view_list_key = 'v',
+    restart_recording_key = 'r',from_first_key = '1', exit_key = 'e'):
+
+        if additional_key.name == add_clicks_key:
+            print("Entering adding clicks mode")
+            self.recording_active = not self.recording_active
+        elif additional_key.name == delete_last_key:
+            self.click_positions_list = self.click_positions_list[:-1]
+            print(f"Deleting {self.click_positions_list[-1]}")
+        elif additional_key.name == pause_resume_key:
+            return True
+        elif additional_key.name == view_list_key:
+            with self.list_lock:
+                print(self.click_positions_list)
+        elif additional_key.name == restart_recording_key:
+            print("Restarting recording")
+            with self.list_lock:
+                self.click_positions_list.clear()
+        elif additional_key.name == exit_key:
+            sys.exit()
+        return False
+
+    def run_autoclicker(self, exit_key = 'e',pause_resume_key='p'):
         sleep_time = 1
         print(
-            f"Running autoclicker with an interval of {sleep_time} seconds. Press '{pause_resume_key}' to pause/resume. Press '{add_clicks_key}' to add clicks.")
+            f"Running autoclicker with an interval of {sleep_time} seconds. Press '{pause_resume_key}' to pause/resume")
+
         autoclicker_thread = Thread(daemon=True, target=self.run_autoclicker_logic)
         autoclicker_thread.start()
 
         try:
+            additional_key_pressed = False  # After pause key press, an additional flag to track additional key press
+
             while True:
                 event = keyboard.read_event(suppress=True)
                 key = event.name
                 state = event.event_type
 
-                if key == 'p':
+                if key == exit_key:
                     break
                 elif key == pause_resume_key and state == keyboard.KEY_DOWN:
-                    self.autoclicking_active = not self.autoclicking_active
-                    if self.autoclicking_active and self.resume:
-                        print("Autoclicker resumed.")
-                    else:
-                        print("Autoclicker paused.")
-                elif key == add_clicks_key and state == keyboard.KEY_DOWN:
-                    print("autoclicker adding clicks mode")
-                    self.add_clicks_mode = True
-                    self.autoclicking_active = not self.autoclicking_active
-                    self.recording_active = not self.recording_active
-                    print(self.recording_active, "recording active")
-                    print(self.autoclicking_active, "autoclicker active")
-                elif key == 'v' and state == keyboard.KEY_DOWN:
-                    print(self.click_positions_list)
-                elif key == 'd' and state == keyboard.KEY_DOWN:
-                    self.click_positions_list = self.click_positions_list[:-1]
+                    self.toggle_autoclicker()
+                    print("Entering/exiting")
 
+                    # Set the flag and continue reading events to avoid missing additional keys
+                    additional_key_pressed = True
+
+                # Check for additional keys only if the flag is set
+                elif additional_key_pressed and state == keyboard.KEY_DOWN:
+                    self.handle_additional_keys(event)
 
         except KeyboardInterrupt:
             print("Main program stopped.")
@@ -113,27 +144,26 @@ class AutoClicker:
             self.listener.stop()
             self.listener.join()
 
-    def check_key_pressed(self, stop_key='p'):
-        print(f"Press {stop_key} to exit")
-        keyboard.wait(stop_key)
+    # def check_key_pressed(self, stop_key='p'):
+    #     print(f"Press {stop_key} to exit")
+    #     keyboard.wait(stop_key)
 
 if __name__ == "__main__":
     autoclicker = AutoClicker()
 
-    record_thread = Thread(target=autoclicker.record_click_positions)
-    autoclick_thread = Thread(daemon=True, target=autoclicker.run_autoclicker)
-    exit_thread = Thread(target=autoclicker.check_key_pressed)
+    record_thread = Thread(daemon= True, target=autoclicker.record_click_positions)
 
-    try:
+    try: #autoclick.runautoclicker
         print("Recording beginning")
         record_thread.start()
         record_thread.join()
 
         print("Autoclick beginning")
-        exit_thread.start()
-        autoclick_thread.start()
-        exit_thread.join()
-
+       # exit_thread.start()
+        autoclicker.run_autoclicker()
+       # exit_thread.join()
+# the autoclicekr is already doing the work so can just end the program from thr autoclicker thread
+    # will use a thread for record but not for autoclick
     except KeyboardInterrupt:
         print("CTRL+C detected. Stopping program.")
 
